c16dcb9840a9701c47f61f265ecd7e3048b3c41b
ayubxon <ayubxonqosimov60@gmail.com>
2025-06-06 22:08:35 +0500

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_learn/calculator/calculator.dart';
-// import 'package:flutter_learn/pomodoro_timer/pomodoro.dart';
 import 'package:flutter_learn/tic_tac_toe/tic_tac_toe.dart';
 
 void main() {
